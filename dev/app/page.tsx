7905e3500c232da5d3662174713bc81fafd3eb45
Dhiraj Kushwaha <dhirajkushwaha0714@gmail.com>
2025-09-18 09:17:17 +0000

    rc

--- dev/app/page.tsx
+++ dev/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     
     <div>
 
-        <div className="" >
-          <div>
-            <img src="" alt="" />
+        <div className="flex" >
+          <div className="w-1/2" >
+            <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
 
           </div>
-          <div>
+          <div className="w-1/2" >
             <h1>
               Hello there 👋
             </h1>
@@ -18,6 +18,10 @@ export default function Home() {
             <h1>
               I am Dhiraj Kushwaha
             </h1>
+            <p>
+              Full Stack Developer. NextJs, Flutter & Django Developer.
+            </p>
+
           </div>
         </div>
 
