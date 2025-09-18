831734335e7d19276d0a640e56ccaa3b0d69c4f3
Dhiraj Kushwaha <dhirajkushwaha0714@gmail.com>
2025-09-18 09:31:50 +0000

    rc

--- dev/app/page.tsx
+++ dev/app/page.tsx
@@ -5,41 +5,60 @@ export default function Home() {
 
     <div>
 
-      <div className="flex" >
-        <div className="w-1/2" >
-          <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
+
+      <section className="bg-white text-black" >
+        <div className="container" >
+          <div className="flex" >
+            <div className="w-1/2" >
+              <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
+
+            </div>
+            <div className="w-1/2" >
+              <h1>
+                Hello there 👋
+              </h1>
+
+              <h1>
+                I am Dhiraj Kushwaha
+              </h1>
+              <p>
+
+                Currently a 2nd-year undergraduate student at IIIT Kota, specializing in Artificial Intelligence and Data Engineering.
+                Passionate Full Stack Developer with expertise in the MERN stack, Next.js, Flutter, and Django.
+
+                I work as a freelance developer Know More.
+                
+                I also manage the official website of IIIT Kota’s Alumni Cell Click Here to know more. 
+              </p>
+
+            </div>
+          </div>
 
         </div>
-        <div className="w-1/2" >
-          <h1>
-            Hello there 👋
-          </h1>
 
-          <h1>
-            I am Dhiraj Kushwaha
-          </h1>
-          <p>
+      </section>
 
-            Currently a 2nd-year undergraduate student at IIIT Kota, specializing in Artificial Intelligence and Data Engineering.
-            Passionate Full Stack Developer with expertise in the MERN stack, Next.js, Flutter, and Django.
 
-            I work as a freelance developer Know More.
+      <section>
+        <div>
+
+            <h1>Checkout my recently Shipped Projects: </h1>
             
-            I also manage the official website of IIIT Kota’s Alumni Cell Click Here to know more. 
-          </p>
+            <div>
+                <a href=""> 
+                  <div>
+                      TykePe
+                  </div>
+                </a>  
+
+            </div>
+
+            
+
 
         </div>
-      </div>
 
-
-
-      <div>
-
-          <h1>Checkout my recently Shipped Projects: </h1>
-
-          
-
-      </div>
+      </section>
 
     </div>
 
