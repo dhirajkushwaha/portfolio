6ac097c537942f78248125ba278d22a5aff0c685
Dhiraj Kushwaha <dhirajkushwaha0714@gmail.com>
2025-09-18 09:25:54 +0000

    rc

--- dev/app/page.tsx
+++ dev/app/page.tsx
@@ -2,28 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
+
     <div>
 
-        <div className="flex" >
-          <div className="w-1/2" >
-            <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
+      <div className="flex" >
+        <div className="w-1/2" >
+          <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
 
-          </div>
-          <div className="w-1/2" >
-            <h1>
-              Hello there 👋
-            </h1>
-
-            <h1>
-              I am Dhiraj Kushwaha
-            </h1>
-            <p>
-              Full Stack Developer. NextJs, Flutter & Django Developer.
-            </p>
-
-          </div>
         </div>
+        <div className="w-1/2" >
+          <h1>
+            Hello there 👋
+          </h1>
+
+          <h1>
+            I am Dhiraj Kushwaha
+          </h1>
+          <p>
+
+            Currently a 2nd-year undergraduate student at IIIT Kota, specializing in Artificial Intelligence and Data Engineering.
+            Passionate Full Stack Developer with expertise in the MERN stack, Next.js, Flutter, and Django.
+
+            I work as a freelance developer Know More.
+            
+            I also manage the official website of IIIT Kota’s Alumni Cell Click Here to know more. 
+          </p>
+
+        </div>
+      </div>
+
+
+
+      <div>
+
+          <h1>Checkout my recently Shipped Projects: </h1>
+
+          
+
+      </div>
 
     </div>
 
