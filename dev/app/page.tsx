b03251b25f1e4c3f390a14e3fc29321d32dd339d
Dhiraj Kushwaha <dhirajkushwaha0714@gmail.com>
2025-09-18 10:07:09 +0000

    rc

--- dev/app/page.tsx
+++ dev/app/page.tsx
@@ -6,29 +6,34 @@ export default function Home() {
     <div>
 
 
-      <section className="bg-white text-black" >
-        <div className="container" >
-          <div className="flex" >
-            <div className="w-1/2" >
-              <img src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
+      <section className="bg-white text-black h-[80vh]" >
+        <div className="container mx-auto" >
+          <div className="flex items-center justify-between mx-auto max-w-5xl" >
+            <div className="w-1/2 " >
+              <img className="rounded-[10px] overflow-hidden" src="https://alumni.iiitkota.ac.in/assets/dhirajKushwaha-DT7b_YFB.png" alt="" />
 
             </div>
             <div className="w-1/2" >
-              <h1>
+              <h1 className="text-2xl" >
                 Hello there 👋
               </h1>
 
-              <h1 className="fontA">
+              <h1 className="mt-2 fontA font-medium text-4xl">
                 I am Dhiraj Kushwaha
               </h1>
-              <p>
+              <p className="mt-2">
 
                 Currently a 2nd-year undergraduate student at IIIT Kota, specializing in Artificial Intelligence and Data Engineering.
                 Passionate Full Stack Developer with expertise in the MERN stack, Next.js, Flutter, and Django.
-
-                I work as a freelance developer <a href="./about">Know More</a> 
                 
-                I also manage the official website of IIIT Kota’s Alumni Cell <a href="https://alumni.iiitkota.ac.in/about"></a> Click Here to know more. 
+              </p>
+              <p>
+                I work as a freelance developer <a href="./about">Know More</a> 
+              
+              </p>
+
+              <p>
+                I also manage the official website of IIIT Kota’s Alumni Cell <a target="_blank" href="https://alumni.iiitkota.ac.in/about">Click Here to know more. </a> 
               </p>
 
             </div>
@@ -40,18 +45,23 @@ export default function Home() {
 
 
       <section>
-        <div>
+        <div className="container mx-auto" >
 
-            <h1>Checkout my recently Shipped Projects: </h1>
-            
-            <div>
-                <a href=""> 
-                  <div>
-                      TykePe
-                  </div>
-                </a>  
+            <div className="max-w-5xl mx-auto" >
+              <h1 className="text-center poppins font-medium text-4xl " >Checkout my recently Shipped Projects: </h1>
+              
+              <div>
+                  <a  target="_blank" href="https://tykepe.vercel.app/"> 
+                    <div className="bg-gray-200 p-2 rounded-[10px] w-xl" >
+                        TykePe
+                    </div>
+                  </a>  
+
+              </div>
 
             </div>
+
+
 
             
 
---
+++ dev/components/Header.tsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Header(){
+    return(
+        <div className="py-3" >
+            
+            <div className="mx-auto text-lg max-w-5xl flex gap-4">
+                <a href="./">Home</a>
+                <a href="./about">About</a>
+                <a href="./contact">Contact</a>
+
+
+            </div>
+
+        </div>
+    )
+}
